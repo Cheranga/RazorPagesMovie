7bd3e726ddac61c22cd989065f7a4155bdf6207d
cheranga <cheranga@gmail.com>
2018-05-21 21:33:54 +1000

    Initial commit

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+            
+        }
+    }
+}
